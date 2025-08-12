7eead1b4f5bc53498a5fe9f61dbfcfe8424fa2cc
GaddamRajasri <gaddamrajasri31@gmail.com>
2025-08-12 12:12:17 +0530

    maven project by 31

---
+++ src/main/java/SEproject/manikanta6631/App.java
@@ -0,0 +1,10 @@
+package SEproject.manikanta6631;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("first java program by manikanta(6631)");
+    }
+}
